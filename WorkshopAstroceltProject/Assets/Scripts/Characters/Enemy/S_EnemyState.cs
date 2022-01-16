762c923e0158c6d71dc0f59ca4041a0f3654932e
rifhall <riley@kjhalloran.com>
2022-01-16 15:46:24 -0500

    Created Folder Structure and made a mock Scene with a lot of empties. Added some scripts
    Added the outer most folders as discussed in tech documentation. Made a programmer scene to plan out the scene hierarchy. Created global, turnmanager, player, enemy, and p&e attributes and state scripts

---
+++ WorkshopAstroceltProject/Assets/Scripts/Characters/Enemy/S_EnemyState.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_EnemyState : MonoBehaviour
+{
+    private S_Global g_global;
+
+    void Awake()
+    {
+        g_global = S_Global.g_instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
