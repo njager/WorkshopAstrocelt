fc2db6226e8526668046d9b7a175813f7002a71f
Josh Tonner <jtonner@iu.edu>
2022-02-01 10:56:13 -0500

    .SVG Implementation

---
+++ WorkshopAstroceltProject/Assets/Scripts/Managers/S_VectorManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.VectorGraphics;
+using Unity.VectorGraphics.Editor;
+
+public class S_VectorManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- WorkshopAstroceltProject/Assets/Scripts/System/S_Global.cs
+++ WorkshopAstroceltProject/Assets/Scripts/System/S_Global.cs
@@ -18,7 +18,8 @@ public class S_Global : MonoBehaviour
     public S_Enemy g_enemy;
     public S_MapGeneration g_mapManager;
     public S_DrawingManager g_DrawingManager;
-    public S_UIManager g_UIManager; 
+    public S_UIManager g_UIManager;
+    public S_VectorManager g_vectorManager;
 
     [Header("Character States")]
     public bool g_b_playerTurn;
