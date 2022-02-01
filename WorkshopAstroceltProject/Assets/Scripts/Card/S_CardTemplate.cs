13a553e0ac6303a7a31b81ab3668318253996e00
Joshua Tonner <jtonner@iu.edu>
2022-02-01 16:50:59 -0500

    CardStructure started

---
+++ WorkshopAstroceltProject/Assets/Scripts/Card/S_CardTemplate.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_CardTemplate : ScriptableObject
+{
+    public string c_str_cardName;
+    public string c_str_flavorText;
+
+    public int c_i_cardRarity; 
+    public int c_i_attackVal;
+
+
+    [Header("Color Types")]
+    public bool c_b_redColorType;
+    public bool c_b_blueColorType;
+    public bool c_b_greenColorType;
+    public bool c_b_colorlessType; 
+
+    [Header("Effect Types")]
+    public bool c_b_damageEffectType;
+    public bool c_b_healEffectType;
+    public bool c_b_shieldEffectType;
+
+    [Header("Status Effect Triggers")]
+    public bool c_b_bleedStatusEffectType;
+    public bool c_b_poisonStatusEffectType;
+
+    private void Awake()
+    {
+        c_b_damageEffectType = false;
+        c_b_healEffectType = false;
+        c_b_shieldEffectType = false;
+
+        c_b_redColorType = false;
+        c_b_blueColorType = false;
+        c_b_greenColorType = false;
+        c_b_colorlessType = false;
+
+        c_b_bleedStatusEffectType = false;
+        c_b_poisonStatusEffectType = false;
+    }
+
+}
