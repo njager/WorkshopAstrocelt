b2c2642ff8953e05d9d0f591d494b21635c78d60
Josh Tonner <jtonner@iu.edu>
2022-02-03 21:24:33 -0500

    Player Script Works

--- WorkshopAstroceltProject/Assets/Scripts/Characters/Player/S_Player.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Characters/Player/S_Player.cs
@@ -6,14 +6,113 @@ public class S_Player : MonoBehaviour
 {
     private S_Global g_global;
 
-    void Awake()
+    private S_PlayerAttributes p_playerAttributes; 
+
+    /// <summary>
+    /// Basic Start S_Global setup, grabbing playerAttributes; 
+    /// </summary>
+
+    void Start()
     {
         g_global = S_Global.g_instance;
+
+        //p_playerAttributes = g_global.g_p_playerAttributeSheet; 
     }
 
-    // Update is called once per frame
-    void Update()
+
+    /// <summary>
+    /// Trigger function for when the player is attacked
+    /// </summary>
+    /// <param name="_damageValue"></param>
+    
+
+    public void PlayerAttacked(int _damageValue)
     {
-        
+        p_playerAttributes.p_i_health -= _damageValue; 
+    }
+
+
+    /// <summary>
+    /// Trigger function for when the player is healed
+    /// </summary>
+    /// <param name="_healedValue"></param>
+    
+    public void PlayerHealed(int _healedValue)
+    {
+        p_playerAttributes.p_i_health += _healedValue; 
+    }
+
+
+    /// <summary>
+    /// Status Effect Trigger function for when the player is bleeding
+    /// </summary>
+    /// <param name="_statusEffect"></param>
+    /// <param name="bleedingRate"></param>
+    /// <param name="_turnCount"></param>
+   
+    public void PlayerBleeding(bool _statusEffect, float bleedingRate, int _turnCount)
+    {
+        if(_statusEffect == p_playerAttributes.p_b_bleeding)
+        {
+            _statusEffect = p_playerAttributes.p_b_bleeding;
+            if (_statusEffect == false)
+            {
+
+            }
+            else
+            {
+                Debug.Log("Status Already Active!");
+                return;
+            }
+        }
+        else
+        {
+            Debug.Log("Wrong Status Effect!");
+            return; 
+        }
+    }
+
+    public void PlayerStunned(bool _statusEffect, int _stunVal, int _turnCount)
+    {
+        if (_statusEffect == p_playerAttributes.p_b_stunned)
+        {
+            _statusEffect = p_playerAttributes.p_b_stunned;
+            if (_statusEffect == false)
+            {
+
+            }
+            else
+            {
+                Debug.Log("Status Already Active!");
+                return;
+            }
+        }
+        else
+        {
+            Debug.Log("Wrong Status Effect!");
+            return;
+        }
+    }
+
+    public void PlayerPoisoned(bool _statusEffect,int _poisonVal, int _turnCount)
+    {
+        if (_statusEffect == p_playerAttributes.p_b_stunned)
+        {
+            _statusEffect = p_playerAttributes.p_b_stunned;
+            if (_statusEffect == false)
+            {
+
+            }
+            else
+            {
+                Debug.Log("Status Already Active!");
+                return; 
+            }
+        }
+        else
+        {
+            Debug.Log("Wrong Status Effect!");
+            return; 
+        }
     }
 }
