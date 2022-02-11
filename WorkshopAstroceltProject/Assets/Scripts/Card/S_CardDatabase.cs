bef6255390b4b685739dcd4559b6755d8127e10d
Josh Tonner <jtonner@iu.edu>
2022-02-10 22:03:50 -0500

    Push

--- WorkshopAstroceltProject/Assets/Scripts/Card/S_CardDatabase.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Card/S_CardDatabase.cs
@@ -1,32 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq; 
 
 public class S_CardDatabase : MonoBehaviour
 {
-    private S_Global g_global; 
+    //Ask will how to iterate objects into a list
+    private S_Global g_global;
 
     public int i_cardCount;
 
     [Header("Card Prefabs")]
     public GameObject cardPrefab0; //Wicked Strike, ID 0
-    public GameObject cardPrefab1; //Wicked Strike, ID 0
-    public GameObject cardPrefab2; //Wicked Strike, ID 0
-    public GameObject cardPrefab3; //Wicked Strike, ID 0
+    public GameObject cardPrefab1; //Lasting Blow, ID 1
+    public GameObject cardPrefab2; //Magic Armor, ID 2
 
-    Dictionary<GameObject, int> dict_CardDatabase = new Dictionary<GameObject, int>();
+    Dictionary<int, GameObject> dict_CardDatabase = new Dictionary<int, GameObject>();
 
     void Awake()
     {
-        for (int i = 0; i < i_cardCount; i++)
-        {
-            dict_CardDatabase.Add(cardPrefab0, 0);
-        }
+        g_global = S_Global.Instance;
+        dict_CardDatabase.Add(0, cardPrefab0);
+        dict_CardDatabase.Add(1, cardPrefab0);
+        dict_CardDatabase.Add(2, cardPrefab0);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Function will return the game object when given an indice
+    /// -Josh
+    /// </summary>
+    /// <param name="_index"></param>
+    /// <returns></returns>
+    public GameObject GetCard(int _index)
     {
-
+        GameObject _returnCard = dict_CardDatabase[_index];
+        return _returnCard;
     }
 }
