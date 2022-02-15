a8dbd623c5e0945f918904da296e5641169dad5a
Josh Tonner <jtonner@iu.edu>
2022-02-15 10:07:36 -0500

    Adapting card structure to "Build" a card

--- WorkshopAstroceltProject/Assets/Scripts/Card/S_Card.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Card/S_Card.cs
@@ -5,9 +5,10 @@ using TMPro;
 
 public class S_Card : MonoBehaviour
 {
-    private ScriptableObject cardTemplate;
+    public ScriptableObject c_cardTemplate;
     private S_Global global;
 
+    // Index from database;
     public int c_i_cardIndex; 
 
     [Header("Card Basics")]
@@ -16,17 +17,29 @@ public class S_Card : MonoBehaviour
     public string c_str_bodyText;
     public string c_str_flavorText;
 
+    // Number values
     public int c_i_energyCost; //Cast to a string for energy cost text
-    public int c_i_effectValue;
-
+    public int c_i_effectValue; // Takes precedence for int calculations 
+    
+    // Rarity
     public int c_i_cardRarity;
-     
+
+    [Header("Card Graphic Options")]
+    public GameObject c_childRedGraphic; // If Red, toggle
+    public GameObject c_childBlueGraphic; // If Blue, toggle
+    public GameObject c_childYellowGraphic; // If Yellow, toggle
+    public GameObject c_childWhiteGraphic; // If White, toggle
+
     [Header("Card Graphic References")]
-    public GameObject c_childGraphic; // The graphic itself
     public TextMeshProUGUI c_tx_header; // Header Textbox
     public TextMeshProUGUI c_tx_body; // Body Text Box
     public TextMeshProUGUI c_tx_flavor; // Flavor Text Box
     public TextMeshProUGUI c_tx_energyCost; // Energy Cost for card
+
+    [Header("Num of Characters Affected")]
+    public bool c_b_affectsSelf;
+    public bool c_b_affectsOne;
+    public bool c_b_affectsTwo;
 
     [Header("Color Types")]
     public bool c_b_redColorType;
@@ -34,15 +47,26 @@ public class S_Card : MonoBehaviour
     public bool c_b_greenColorType;
     public bool c_b_whiteColorType;
 
-    [Header("Effect Types")]
-    public bool c_b_damageEffectType;
-    public bool c_b_healEffectType;
-    public bool c_b_shieldEffectType;
+    [Header("Main Effect Types")]
+    public bool c_b_damageMainEffect;
+    public bool c_b_healMainEffect;
+    public bool c_b_shieldMainEffect;
+    public bool c_b_uniqueMainEffect;
 
     [Header("Status Effect Triggers")]
-    public bool c_b_bleedStatusEffectType;
-    public bool c_b_stunnedStatusEffectType;
-    public bool c_b_poisonStatusEffectType;
+    public bool c_b_noEffect;
+    public bool c_b_bleedStatusEffect;
+    public bool c_b_stunStatusEffect;
+    public bool c_b_poisonStatusEffect;
+    public bool c_b_empowerStatusEffect;
+    public bool c_b_luckyStatusEffect;
+    public bool c_b_restrainStatusEffect;
+    public bool c_b_burnStatusEffect;
+    public bool c_b_shockStatusEffect;
+
+    [Header("Potential Status Effect Values")]
+    public float c_f_damagePercentage; // If not usinf effect value, use this
+    public float c_f_turnCount;
 
     private void Awake()
     {
@@ -51,6 +75,11 @@ public class S_Card : MonoBehaviour
     
     private void Start()
     {
-        
+        FetchCardData(c_cardTemplate);
+    }
+
+    public void FetchCardData(ScriptableObject _cardData)
+    {
+
     }
 }
