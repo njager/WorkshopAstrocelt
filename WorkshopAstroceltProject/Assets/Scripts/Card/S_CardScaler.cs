598c4705b1be10d85eb630c8cd3a470a89a536c5
Josh Tonner <jtonner@iu.edu>
2022-03-03 12:15:37 -0500

    S_CardScaler filled a little, minor changes to card template

--- WorkshopAstroceltProject/Assets/Scripts/Card/S_CardScaler.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Card/S_CardScaler.cs
@@ -1,13 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class S_CardScaler : MonoBehaviour
 {
+    private S_Global global;
+    //Card to eventually modify the rest of the elements of the card object, including it's rigidbody
+
+    [Header("Other card elements")]
+    public S_Card c_card;
+    public BoxCollider2D c_BoxCollider2D;
+
+    [Header("Card Graphics")]
+    public GameObject c_redArtGraphic;
+    public GameObject c_blueArtGraphic;
+    public GameObject c_yellowArtGraphic;
+    public GameObject c_whiteArtGraphic;
+
+    [Header("Text Objects")]
+    public GameObject c_headerTextObject;
+    public GameObject c_bodyTextObject;
+    public GameObject c_energyCostTextObject;
+    public GameObject c_flavorTextObject;
+
+    [Header("Text Components")]
+    public TextMeshProUGUI c_tx_headerText;
+    public TextMeshProUGUI c_tx_bodyText;
+    public TextMeshProUGUI c_tx_energyCostText;
+    public TextMeshProUGUI c_tx_flavorText;
+
+    //Feel free to change any of these elements around, I just thought they may be useful - Josh
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        global = S_Global.Instance;
     }
 
     // Update is called once per frame
