9a6f23a74a49ef6c8128b301f6e561a96c498610
Josh Tonner <jtonner@iu.edu>
2022-03-05 11:42:25 -0500

    Adding card playing ability

--- WorkshopAstroceltProject/Assets/Scripts/Characters/S_CharacterCardInterface.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Characters/S_CharacterCardInterface.cs
@@ -22,7 +22,16 @@ public class S_CharacterCardInterface : MonoBehaviour, IDropHandler
 
     void Start()
     {
-
+        if(tag == "Player") // If on player, set bools accordingly
+        {
+            p_b_attachedToPlayer = true;
+            p_b_attachedToEnemy = false;
+        }
+        if(tag == "Enemy") // If on an enemy, set bools accordingly
+        {
+            p_b_attachedToPlayer = true;
+            p_b_attachedToEnemy = false;
+        }
     }
 
     public void OnDrop(PointerEventData _eventData)
@@ -44,11 +53,49 @@ public class S_CharacterCardInterface : MonoBehaviour, IDropHandler
                 }
                 else if(p_b_attachedToEnemy == true) // I like to make bool checks clear in what they are checking, but could be optimized
                 {
-                    if (c_cardData.c_b_affectsPlayer == true) //check to see if it affects enemy
+                    if (c_cardData.c_b_affectsOne == true) //check to see if it affects enemy
                     {
                         if (c_cardData.c_b_shieldMainEffect == true) //check to see if it's an attack card
                         {
                             c_cardData.PlayCard(g_global.g_player.gameObject);
+                        }
+                    }
+                    else if(c_cardData.c_b_affectsAll == true) // Activate on all enemies
+                    {
+                        if(g_global.g_enemyAttributeSheet1 != null) // Activate on all enemies then
+                        {
+                            if (g_global.g_enemyState.e_b_enemy1Dead == false)
+                            {
+                                c_cardData.PlayCard(g_global.g_enemyState.enemy1.gameObject);
+                            }
+                        }
+                        if (g_global.g_enemyAttributeSheet2 != null) // enemy 2
+                        {
+                            if (g_global.g_enemyState.e_b_enemy2Dead == false)
+                            {
+                                c_cardData.PlayCard(g_global.g_enemyState.enemy2.gameObject);
+                            }
+                        }
+                        if (g_global.g_enemyAttributeSheet3 != null) // Enemy 3
+                        {
+                            if (g_global.g_enemyState.e_b_enemy3Dead == false)
+                            {
+                                c_cardData.PlayCard(g_global.g_enemyState.enemy3.gameObject);
+                            }
+                        }
+                        if (g_global.g_enemyAttributeSheet4 != null) // Enemy 4
+                        {
+                            if (g_global.g_enemyState.e_b_enemy4Dead == false)
+                            {
+                                c_cardData.PlayCard(g_global.g_enemyState.enemy4.gameObject);
+                            }
+                        }
+                        if (g_global.g_enemyAttributeSheet5 != null) // Enemy 5
+                        {
+                            if (g_global.g_enemyState.e_b_enemy5Dead == false)
+                            {
+                                c_cardData.PlayCard(g_global.g_enemyState.enemy5.gameObject);
+                            }
                         }
                     }
                 }
