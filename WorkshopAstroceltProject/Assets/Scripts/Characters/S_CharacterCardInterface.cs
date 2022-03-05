bdb1b6e803b09df349f50cf2621da220f432f2f8
Josh Tonner <jtonner@iu.edu>
2022-03-05 11:33:56 -0500

    Distinguishing interactable objects

--- WorkshopAstroceltProject/Assets/Scripts/Characters/S_CharacterCardInterface.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Characters/S_CharacterCardInterface.cs
@@ -12,9 +12,17 @@ public class S_CharacterCardInterface : MonoBehaviour, IDropHandler
     private S_Card c_cardData;
     private S_Global g_global;
 
+    private bool p_b_attachedToPlayer;
+    private bool p_b_attachedToEnemy; 
+
     void Awake()
     {
         p_playerRectTransform = GetComponent<RectTransform>();
+    }
+
+    void Start()
+    {
+
     }
 
     public void OnDrop(PointerEventData _eventData)
@@ -24,11 +32,24 @@ public class S_CharacterCardInterface : MonoBehaviour, IDropHandler
             c_cardData = _eventData.pointerDrag.GetComponent<S_Card>();
             if(g_global.g_ConstellationManager.i_energyCount >= c_cardData.c_i_energyCost)
             {
-                if (c_cardData.c_b_affectsPlayer == true) //check to see if it affects player
+                if (p_b_attachedToPlayer == true) //check to see if this object is the player
                 {
-                    if (c_cardData.c_b_shieldMainEffect == true) //check to see if it's a shield card
+                    if (c_cardData.c_b_affectsPlayer == true) //check to see if it affects player
                     {
-                        c_cardData.PlayCard(g_global.g_player.gameObject);
+                        if (c_cardData.c_b_shieldMainEffect == true) //check to see if it's a shield card
+                        {
+                            c_cardData.PlayCard(g_global.g_player.gameObject);
+                        }
+                    }
+                }
+                else if(p_b_attachedToEnemy == true) // I like to make bool checks clear in what they are checking, but could be optimized
+                {
+                    if (c_cardData.c_b_affectsPlayer == true) //check to see if it affects enemy
+                    {
+                        if (c_cardData.c_b_shieldMainEffect == true) //check to see if it's an attack card
+                        {
+                            c_cardData.PlayCard(g_global.g_player.gameObject);
+                        }
                     }
                 }
             }
