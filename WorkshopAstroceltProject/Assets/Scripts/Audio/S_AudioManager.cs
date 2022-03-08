afdab8f02a33bdbd7e92dda20766a52b7f0d4262
Joshua Tonner <jtonner@iu.edu>
2022-03-08 11:58:59 -0500

    ResizedDeck

--- WorkshopAstroceltProject/Assets/Scripts/Audio/S_AudioManager.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Audio/S_AudioManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+public class S_AudioManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
