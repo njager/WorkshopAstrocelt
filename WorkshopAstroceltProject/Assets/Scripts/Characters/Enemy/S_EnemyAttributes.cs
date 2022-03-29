c1c014677eb00a32e9a43c8b2bfb73205743a9cc
Joshua Tonner <jtonner@iu.edu>
2022-03-29 15:23:34 -0400

    Enemy Changes

--- WorkshopAstroceltProject/Assets/Scripts/Characters/Enemy/S_EnemyAttributes.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Characters/Enemy/S_EnemyAttributes.cs
@@ -20,7 +20,9 @@ public class S_EnemyAttributes : MonoBehaviour
     public int e_i_shield;
     public int e_i_shieldMax;
 
-    public int e_i_enemyDamageValue;
+    public int e_i_enemyMinDamageRange;
+    public int e_i_enemyMaxDamageRange;
+    public int e_i_enemyDamageValue; 
 
     public float e_f_challengeRating;
 
@@ -29,7 +31,9 @@ public class S_EnemyAttributes : MonoBehaviour
 
     // Add more enemies to toggle on and off as needed
     [Header("Enemy Type Bools")]
-    public bool e_b_enemyIsLumberjack;
+    public bool e_b_Lumberjack;
+    public bool e_b_Magician;
+    public bool e_b_Brawler;
 
     [Header("Status Effects")]
     public bool e_b_poisoned;
@@ -42,6 +46,7 @@ public class S_EnemyAttributes : MonoBehaviour
     public bool e_b_shocked;
 
     //It's attached enemy script
+    [Header("Attached Enemy Script")]
     public S_Enemy e_enemy;
 
     public void Awake()
@@ -49,7 +54,7 @@ public class S_EnemyAttributes : MonoBehaviour
         g_global = S_Global.Instance; 
 
         //Inform Global
-        InstanceVariables();
+        //InstanceVariables();
         e_enemy = gameObject.GetComponent<S_Enemy>();
     }
 
