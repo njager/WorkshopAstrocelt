a2103e7d4a504e8c15b0ff0d4ac96b26c7f140cc
Josh Tonner <jtonner@iu.edu>
2022-03-30 18:12:17 -0400

    Functionality work

--- WorkshopAstroceltProject/Assets/Scripts/UI/S_StarPopUp.cs
+++ WorkshopAstroceltProject/Assets/Scripts/UI/S_StarPopUp.cs
@@ -5,22 +5,30 @@ using System.Linq;
 using TMPro;
 using CodeMonkey.Utils;
 using DG.Tweening;
+using Unity.VectorGraphics;
+using Unity.VectorGraphics.Editor;
 
 public class S_StarPopUp : MonoBehaviour
 {
     private S_Global g_global;
     private bool b_deletionTimerFlag;
+    private SVGImage colorImage;
 
     [Header("Color Bools")]
-    [SerializeField] bool b_useRed;
-    [SerializeField] bool b_useYellow;
-    [SerializeField] bool b_useBlue;
+    [SerializeField] bool b_redPopup;
+    [SerializeField] bool b_bluePopup;
+    [SerializeField] bool b_yellowPopup;
 
     [Header("Deletion Timer Attributes")]
     [SerializeField] float f_disappearTimer;
     [SerializeField] float f_destroyTimer = 1f;
     [SerializeField] float f_doFadeAlpha;
     [SerializeField] float f_doFadeDuration;
+
+    [Header("Color Graphics")]
+    public GameObject redColorGraphic;
+    public GameObject blueColorGraphic;
+    public GameObject yellowColorGraphic;
 
     /// <summary>
     /// Set up global, add to list, and deletion timer
@@ -38,7 +46,7 @@ public class S_StarPopUp : MonoBehaviour
         f_disappearTimer -= Time.deltaTime;
         if (f_disappearTimer < 0)
         {
-            tx_baseTextMesh.DOFade(f_doFadeAlpha, f_doFadeDuration);
+            colorImage.DOFade(f_doFadeAlpha, f_doFadeDuration);
             f_destroyTimer -= Time.deltaTime;
             if (f_destroyTimer < 0)
             {
@@ -47,6 +55,27 @@ public class S_StarPopUp : MonoBehaviour
         }
         b_deletionTimerFlag = true;
         yield return b_deletionTimerFlag == true;
+    }
+
+    public void SetGraphic(string _color)
+    {
+        if(_color == "red")
+        {
+
+        }
+        if(_color == "blue")
+        {
+
+        }
+        if(_color == "yellow")
+        {
+
+        }
+    }
+
+    public void DeletePopup()
+    {
+        StartCoroutine(DeletionTimer());
     }
 
 }
