7b6ae6c76dcb10c27a03aab6588cc7bfa4a9d967
Josh Tonner <jtonner@iu.edu>
2022-03-31 15:47:18 -0400

    Map random picking

--- WorkshopAstroceltProject/Assets/Scripts/Drawing/S_MapGeneration.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Drawing/S_MapGeneration.cs
@@ -28,6 +28,9 @@ public class S_MapGeneration : MonoBehaviour
     public GameObject map7;
     public GameObject map8;
 
+    [Header("Random Choosing")]
+    public int mp_i_previousMapNum;
+
     //Will grab chunks here
     private void Awake()
     {
@@ -68,9 +71,418 @@ public class S_MapGeneration : MonoBehaviour
 
     ////Temporary Map Toggling and Placements\\\\
     
+
+    /// <summary>
+    /// Chooses maps randomly, with no direct repeats
+    /// Though yes it's dirty
+    /// - Josh
+    /// </summary>
     public void RandomMapSelector()
     {
+        // Choose map int
+        int _mapNumChosen = Random.Range(1, 9);
+        
+        // Toggle maps
 
+        // Map 1
+        if(_mapNumChosen == 1)
+        {
+            if (mp_i_previousMapNum == 1)
+            {
+                // If it was a previous map, choose a new one
+                int _mapNumChosen2 = Random.Range(1, 8);
+                if(_mapNumChosen2 == 1) // Map 2
+                {
+                    Debug.Log("Map 2 Chosen");
+                    Map2();
+                }
+                else if (_mapNumChosen2 == 2) // Map 3
+                {
+                    Debug.Log("Map 3 Chosen");
+                    Map3();
+                }
+                else if (_mapNumChosen2 == 3) // Map 4
+                {
+                    Debug.Log("Map 4 Chosen");
+                    Map4();
+                }
+                else if (_mapNumChosen2 == 4) // Map 5
+                {
+                    Debug.Log("Map 5 Chosen");
+                    Map5();
+                }
+                else if (_mapNumChosen2 == 5) // Map 6
+                {
+                    Debug.Log("Map 6 Chosen");
+                    Map6();
+                }
+                else if (_mapNumChosen2 == 6) // Map 7
+                {
+                    Debug.Log("Map 7 Chosen");
+                    Map7();
+                }
+                else if (_mapNumChosen2 == 7) // Map 8
+                {
+                    Debug.Log("Map 8 Chosen");
+                    Map8();
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Map 1 Chosen");
+            Map1(); 
+        }
+
+        // Map 2
+        if (_mapNumChosen == 2)
+        {
+            if (mp_i_previousMapNum == 2)
+            {
+                // If it was a previous map, choose a new one
+                int _mapNumChosen2 = Random.Range(1, 8);
+                if (_mapNumChosen2 == 1) // Map 1
+                {
+                    Debug.Log("Map 1 Chosen");
+                    Map1();
+                }
+                else if (_mapNumChosen2 == 2) // Map 3
+                {
+                    Debug.Log("Map 3 Chosen");
+                    Map3();
+                }
+                else if (_mapNumChosen2 == 3) // Map 4
+                {
+                    Debug.Log("Map 4 Chosen");
+                    Map4();
+                }
+                else if (_mapNumChosen2 == 4) // Map 5
+                {
+                    Debug.Log("Map 5 Chosen");
+                    Map5();
+                }
+                else if (_mapNumChosen2 == 5) // Map 6
+                {
+                    Debug.Log("Map 6 Chosen");
+                    Map6();
+                }
+                else if (_mapNumChosen2 == 6) // Map 7
+                {
+                    Debug.Log("Map 7 Chosen");
+                    Map7();
+                }
+                else if (_mapNumChosen2 == 7) // Map 8
+                {
+                    Debug.Log("Map 8 Chosen");
+                    Map8();
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Map 2 Chosen");
+            Map2();
+        }
+
+        // Map 3
+        if (_mapNumChosen == 3) 
+        {
+            if (mp_i_previousMapNum == 3)
+            {
+                // If it was a previous map, choose a new one
+                int _mapNumChosen2 = Random.Range(1, 8);
+                if (_mapNumChosen2 == 1) // Map 2
+                {
+                    Debug.Log("Map 2 Chosen");
+                    Map2();
+                }
+                else if (_mapNumChosen2 == 2) // Map 1
+                {
+                    Debug.Log("Map 1 Chosen");
+                    Map1();
+                }
+                else if (_mapNumChosen2 == 3) // Map 4
+                {
+                    Debug.Log("Map 4 Chosen");
+                    Map4();
+                }
+                else if (_mapNumChosen2 == 4) // Map 5
+                {
+                    Debug.Log("Map 5 Chosen");
+                    Map5();
+                }
+                else if (_mapNumChosen2 == 5) // Map 6
+                {
+                    Debug.Log("Map 6 Chosen");
+                    Map6();
+                }
+                else if (_mapNumChosen2 == 6) // Map 7
+                {
+                    Debug.Log("Map 7 Chosen");
+                    Map7();
+                }
+                else if (_mapNumChosen2 == 7) // Map 8
+                {
+                    Debug.Log("Map 8 Chosen");
+                    Map8();
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Map 3 Chosen");
+            Map3();
+        }
+
+        // Map 4
+        if (_mapNumChosen == 4)
+        {
+            if (mp_i_previousMapNum == 4)
+            {
+                // If it was a previous map, choose a new one
+                int _mapNumChosen2 = Random.Range(1, 8);
+                if (_mapNumChosen2 == 1) // Map 2
+                {
+                    Debug.Log("Map 2 Chosen");
+                    Map2();
+                }
+                else if (_mapNumChosen2 == 2) // Map 3
+                {
+                    Debug.Log("Map 3 Chosen");
+                    Map3();
+                }
+                else if (_mapNumChosen2 == 3) // Map 1
+                {
+                    Debug.Log("Map 1 Chosen");
+                    Map1();
+                }
+                else if (_mapNumChosen2 == 4) // Map 5
+                {
+                    Debug.Log("Map 5 Chosen");
+                    Map5();
+                }
+                else if (_mapNumChosen2 == 5) // Map 6
+                {
+                    Debug.Log("Map 6 Chosen");
+                    Map6();
+                }
+                else if (_mapNumChosen2 == 6) // Map 7
+                {
+                    Debug.Log("Map 7 Chosen");
+                    Map7();
+                }
+                else if (_mapNumChosen2 == 7) // Map 8
+                {
+                    Debug.Log("Map 8 Chosen");
+                    Map8();
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Map 4 Chosen");
+            Map4();
+        }
+
+        // Map 5
+        if (_mapNumChosen == 5)
+        {
+            if (mp_i_previousMapNum == 5)
+            {
+                // If it was a previous map, choose a new one
+                int _mapNumChosen2 = Random.Range(1, 8);
+                if (_mapNumChosen2 == 1) // Map 2
+                {
+                    Debug.Log("Map 2 Chosen");
+                    Map2();
+                }
+                else if (_mapNumChosen2 == 2) // Map 3
+                {
+                    Debug.Log("Map 3 Chosen");
+                    Map3();
+                }
+                else if (_mapNumChosen2 == 3) // Map 4
+                {
+                    Debug.Log("Map 4 Chosen");
+                    Map4();
+                }
+                else if (_mapNumChosen2 == 4) // Map 1
+                {
+                    Debug.Log("Map 1 Chosen");
+                    Map1();
+                }
+                else if (_mapNumChosen2 == 5) // Map 6
+                {
+                    Debug.Log("Map 6 Chosen");
+                    Map6();
+                }
+                else if (_mapNumChosen2 == 6) // Map 7
+                {
+                    Debug.Log("Map 7 Chosen");
+                    Map7();
+                }
+                else if (_mapNumChosen2 == 7) // Map 8
+                {
+                    Debug.Log("Map 8 Chosen");
+                    Map8();
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Map 5 Chosen");
+            Map5();
+        }
+
+        // Map 6
+        if (_mapNumChosen == 6)
+        {
+            if (mp_i_previousMapNum == 6)
+            {
+                // If it was a previous map, choose a new one
+                int _mapNumChosen2 = Random.Range(1, 8);
+                if (_mapNumChosen2 == 1) // Map 2
+                {
+                    Debug.Log("Map 2 Chosen");
+                    Map2();
+                }
+                else if (_mapNumChosen2 == 2) // Map 3
+                {
+                    Debug.Log("Map 3 Chosen");
+                    Map3();
+                }
+                else if (_mapNumChosen2 == 3) // Map 4
+                {
+                    Debug.Log("Map 4 Chosen");
+                    Map4();
+                }
+                else if (_mapNumChosen2 == 4) // Map 5
+                {
+                    Debug.Log("Map 5 Chosen");
+                    Map5();
+                }
+                else if (_mapNumChosen2 == 5) // Map 1
+                {
+                    Debug.Log("Map 1 Chosen");
+                    Map1();
+                }
+                else if (_mapNumChosen2 == 6) // Map 7
+                {
+                    Debug.Log("Map 7 Chosen");
+                    Map7();
+                }
+                else if (_mapNumChosen2 == 7) // Map 8
+                {
+                    Debug.Log("Map 8 Chosen");
+                    Map8();
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Map 6 Chosen");
+            Map6();
+        }
+
+        // Map 7
+        if (_mapNumChosen == 7)
+        {
+            if (mp_i_previousMapNum == 7)
+            {
+                // If it was a previous map, choose a new one
+                int _mapNumChosen2 = Random.Range(1, 8);
+                if (_mapNumChosen2 == 1) // Map 2
+                {
+                    Debug.Log("Map 2 Chosen");
+                    Map2();
+                }
+                else if (_mapNumChosen2 == 2) // Map 3
+                {
+                    Debug.Log("Map 3 Chosen");
+                    Map3();
+                }
+                else if (_mapNumChosen2 == 3) // Map 4
+                {
+                    Debug.Log("Map 4 Chosen");
+                    Map4();
+                }
+                else if (_mapNumChosen2 == 4) // Map 5
+                {
+                    Debug.Log("Map 5 Chosen");
+                    Map5();
+                }
+                else if (_mapNumChosen2 == 5) // Map 6
+                {
+                    Debug.Log("Map 6 Chosen");
+                    Map6();
+                }
+                else if (_mapNumChosen2 == 6) // Map 1
+                {
+                    Debug.Log("Map 1 Chosen");
+                    Map1();
+                }
+                else if (_mapNumChosen2 == 7) // Map 8
+                {
+                    Debug.Log("Map 8 Chosen");
+                    Map8();
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Map 7 Chosen");
+            Map7();
+        }
+
+        // Map 8
+        if (_mapNumChosen == 8)
+        {
+            if (mp_i_previousMapNum == 8)
+            {
+                // If it was a previous map, choose a new one
+                int _mapNumChosen2 = Random.Range(1, 8);
+                if (_mapNumChosen2 == 1) // Map 2
+                {
+                    Debug.Log("Map 2 Chosen");
+                    Map2();
+                }
+                else if (_mapNumChosen2 == 2) // Map 3
+                {
+                    Debug.Log("Map 3 Chosen");
+                    Map3();
+                }
+                else if (_mapNumChosen2 == 3) // Map 4
+                {
+                    Debug.Log("Map 4 Chosen");
+                    Map4();
+                }
+                else if (_mapNumChosen2 == 4) // Map 5
+                {
+                    Debug.Log("Map 5 Chosen");
+                    Map5();
+                }
+                else if (_mapNumChosen2 == 5) // Map 6
+                {
+                    Debug.Log("Map 6 Chosen");
+                    Map6();
+                }
+                else if (_mapNumChosen2 == 6) // Map 7
+                {
+                    Debug.Log("Map 7 Chosen");
+                    Map7();
+                }
+                else if (_mapNumChosen2 == 7) // Map 1
+                {
+                    Debug.Log("Map 1 Chosen");
+                    Map1();
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Map 8 Chosen");
+            Map8();
+        }
     }
 
     // Designer Map 1
@@ -89,6 +501,9 @@ public class S_MapGeneration : MonoBehaviour
         map6.SetActive(false);
         map7.SetActive(false);
         map8.SetActive(false);
+
+        // Set previous map for non-repeating
+        mp_i_previousMapNum = 1;
     }
 
     // Designer Map 2
@@ -107,6 +522,9 @@ public class S_MapGeneration : MonoBehaviour
         map6.SetActive(false);
         map7.SetActive(false);
         map8.SetActive(false);
+
+        // Set previous map for non-repeating
+        mp_i_previousMapNum = 2;
     }
 
     // Designer Map 3
@@ -125,6 +543,9 @@ public class S_MapGeneration : MonoBehaviour
         map6.SetActive(false);
         map7.SetActive(false);
         map8.SetActive(false);
+
+        // Set previous map for non-repeating
+        mp_i_previousMapNum = 3;
     }
 
     // Designer Map 4
@@ -143,6 +564,9 @@ public class S_MapGeneration : MonoBehaviour
         map6.SetActive(false);
         map7.SetActive(false);
         map8.SetActive(false);
+
+        // Set previous map for non-repeating
+        mp_i_previousMapNum = 4;
     }
 
     // Designer Map 5
@@ -161,15 +585,14 @@ public class S_MapGeneration : MonoBehaviour
         map6.SetActive(false);
         map7.SetActive(false);
         map8.SetActive(false);
+
+        // Set previous map for non-repeating
+        mp_i_previousMapNum = 5;
     }
 
     // Designer Map 6
     public void Map6()
     {
-        //Move nodestar
-        Vector3 newNodeStarPosition = new Vector3(-1.21f, 4.87f, 0f);
-        nodeStar.gameObject.transform.position = newNodeStarPosition;
-
         //Toggle maps
         map1.SetActive(false);
         map2.SetActive(false);
@@ -179,15 +602,14 @@ public class S_MapGeneration : MonoBehaviour
         map6.SetActive(true);
         map7.SetActive(false);
         map8.SetActive(false);
+
+        // Set previous map for non-repeating
+        mp_i_previousMapNum = 6;
     }
 
     // Designer Map 7
     public void Map7()
     {
-        //Move nodestar
-        Vector3 newNodeStarPosition = new Vector3(-1.21f, 4.87f, 0f);
-        nodeStar.gameObject.transform.position = newNodeStarPosition;
-
         //Toggle maps
         map1.SetActive(false);
         map2.SetActive(false);
@@ -197,15 +619,14 @@ public class S_MapGeneration : MonoBehaviour
         map6.SetActive(false);
         map7.SetActive(true);
         map8.SetActive(false);
+
+        // Set previous map for non-repeating
+        mp_i_previousMapNum = 7;
     }
 
     // Designer Map 8
     public void Map8()
     {
-        //Move nodestar
-        Vector3 newNodeStarPosition = new Vector3(-1.21f, 4.87f, 0f);
-        nodeStar.gameObject.transform.position = newNodeStarPosition;
-
         //Toggle maps
         map1.SetActive(false);
         map2.SetActive(false);
@@ -215,5 +636,8 @@ public class S_MapGeneration : MonoBehaviour
         map6.SetActive(false);
         map7.SetActive(false);
         map8.SetActive(true);
+
+        // Set previous map for non-repeating
+        mp_i_previousMapNum = 8;
     }
 }
