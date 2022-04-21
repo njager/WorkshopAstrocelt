5d324421f0771d72713903bf2cb14d108cbe6e75
rifhall <riley@kjhalloran.com>
2022-04-21 12:03:00 -0400

    removing some card dragger references

--- WorkshopAstroceltProject/Assets/Scripts/Card/S_CardDragger.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Card/S_CardDragger.cs
@@ -31,40 +31,31 @@ public class S_CardDragger : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
 
     public void OnDrag(PointerEventData _eventData)
     {
-        //only trigger if your the front card in the hand
-        if (this.gameObject == g_global.ls_p_playerHand[0])
+        if (transformCounter == 0)
         {
-            if (transformCounter == 0)
-            {
-                c_v3_initialPosition = GetComponent<Transform>().position;
-                transformCounter++; // Increment counter to make this happen only once
-            }
-            c_cardTransform.anchoredPosition += _eventData.delta / g_global.g_UIManager.greyboxCanvas.GetComponent<Canvas>().scaleFactor; // Take the change in mouse position and divide it by the size of the box it's in
+            c_v3_initialPosition = GetComponent<Transform>().position;
+            transformCounter++; // Increment counter to make this happen only once
         }
+        c_cardTransform.anchoredPosition += _eventData.delta / g_global.g_UIManager.greyboxCanvas.GetComponent<Canvas>().scaleFactor; // Take the change in mouse position and divide it by the size of the box it's in
+        
     }
 
     public void OnEndDrag(PointerEventData _eventData)
     {
-        if (this.gameObject == g_global.ls_p_playerHand[0])
-        {
-            Debug.Log(c_card.c_str_cardName + " : This card ended drag and now is in reset position");
-            //reset the card if it didnt trigger the CharacterCardInterface
-            //_eventData.pointerDrag.GetComponent<S_Card>().ResetPosition();
+        Debug.Log(c_card.c_str_cardName + " : This card ended drag and now is in reset position");
+        //reset the card if it didnt trigger the CharacterCardInterface
+        //_eventData.pointerDrag.GetComponent<S_Card>().ResetPosition();
 
-            //start the audio
-            if (g_global.b_firstCard)
-            {
-                g_global.b_firstCard = false;
-                FMODUnity.StudioEventEmitter _gameMusic = g_global.a_audioPlayer.GetComponent<FMODUnity.StudioEventEmitter>();
-                _gameMusic.SetParameter("Music_Combat_01", 1);
-            }
-
-            //reset position
-            c_card.ResetPosition();
-        }
-        else
+        //start the audio
+        if (g_global.b_firstCard)
         {
-            return; 
+            g_global.b_firstCard = false;
+            FMODUnity.StudioEventEmitter _gameMusic = g_global.a_audioPlayer.GetComponent<FMODUnity.StudioEventEmitter>();
+            _gameMusic.SetParameter("Music_Combat_01", 1);
         }
+
+        //reset position
+        c_card.ResetPosition();
+
     }
 }
