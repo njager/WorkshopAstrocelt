8d83251da7831a18c03320bc483a846ea1a67c6b
Josh Tonner <jtonner@iu.edu>
2022-07-18 18:01:31 -0400

    Turn effect progress

--- WorkshopAstroceltProject/Assets/Scripts/Managers/S_TurnEffectManager.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Managers/S_TurnEffectManager.cs
@@ -7,53 +7,48 @@ public class S_TurnEffectManager : MonoBehaviour // Enemy Turn Highlighting Scri
     private S_Global g_global;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         g_global = S_Global.Instance;
     }
 
     /// <summary>
-    /// Trigger function for helper functions in this script for enemy 1
+    /// Major trigger function for helper functions in this script for the current enemy turn
     /// - Josh
     /// </summary>
-    public void Enemy1TurnEffects() 
+    public void EnemyTurnEffects() 
     {
-        g_global.g_UIManager.EnemyHighlightToggle(g_global.g_enemyAttributeSheet1);
-    }
+        if (g_global.g_enemyState.CurrentEnemyTurnNumber() == 1) // Enemy 1 Turn Effects
+        {
+            // Highlight Circle Toggle
+            g_global.g_UIManager.EnemyHighlightToggle(g_global.g_enemyAttributeSheet1);
+        }
+        else if (g_global.g_enemyState.CurrentEnemyTurnNumber() == 1) // Enemy 2 Turn Effects
+        {
+            // Highlight Circle Toggle
+            g_global.g_UIManager.EnemyHighlightToggle(g_global.g_enemyAttributeSheet2);
+        }
+        else if (g_global.g_enemyState.CurrentEnemyTurnNumber() == 1) // Enemy 3 Turn Effects
+        {
+            // Highlight Circle Toggle
+            g_global.g_UIManager.EnemyHighlightToggle(g_global.g_enemyAttributeSheet3);
+        }
+        else if (g_global.g_enemyState.CurrentEnemyTurnNumber() == 1) // Enemy 4 Turn Effects
+        {
+            // Highlight Circle Toggle
+            g_global.g_UIManager.EnemyHighlightToggle(g_global.g_enemyAttributeSheet4);
+        }
+        else if (g_global.g_enemyState.CurrentEnemyTurnNumber() == 1) // Enemy 5 Turn Effects 
+        {
+            // Highlight Circle Toggle
+            g_global.g_UIManager.EnemyHighlightToggle(g_global.g_enemyAttributeSheet5);
+        }
+        else 
+        {
+            int _debugNumber = g_global.g_enemyState.CurrentEnemyTurnNumber();
+            Debug.Log("CurrentEnemyTurnNumber likely returned zero: " + _debugNumber);
+            Debug.Log("If not then, check this script rather then S_EnemyState");
+        }
 
-    /// <summary>
-    /// Trigger function for helper functions in this script for enemy 2
-    /// - Josh
-    /// </summary>
-    public void Enemy2TurnEffects()
-    {
-        g_global.g_UIManager.EnemyHighlightToggle(g_global.g_enemyAttributeSheet2);
-    }
-
-    /// <summary>
-    /// Trigger function for helper functions in this script for enemy 3
-    /// - Josh
-    /// </summary>
-    public void Enemy3TurnEffects()
-    {
-        g_global.g_UIManager.EnemyHighlightToggle(g_global.g_enemyAttributeSheet3);
-    }
-
-    /// <summary>
-    /// Trigger function for helper functions in this script for enemy 4
-    /// - Josh
-    /// </summary>
-    public void Enemy4TurnEffects()
-    {
-        g_global.g_UIManager.EnemyHighlightToggle(g_global.g_enemyAttributeSheet4);
-    }
-
-    /// <summary>
-    /// Trigger function for helper functions in this script for enemy 5
-    /// - Josh
-    /// </summary>
-    public void Enemy5TurnEffects()
-    {
-        g_global.g_UIManager.EnemyHighlightToggle(g_global.g_enemyAttributeSheet5);
     }
 }
