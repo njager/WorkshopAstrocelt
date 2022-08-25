d1e2d48b556a52594756fb710dfd4a81636944ad
Joshua Tonner <jtonner@iu.edu>
2022-08-25 15:47:49 -0400

    Implemented changes according to will, cleaned up scripts

--- WorkshopAstroceltProject/Assets/Scripts/Characters/Enemy/S_Enemy.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Characters/Enemy/S_Enemy.cs
@@ -32,9 +32,6 @@ public class S_Enemy : MonoBehaviour
     [Header("Intent Duration Value")]
     public float f_intentDuration;
 
-    // Enemy Turn Delegate
-    private S_EventManager.EnemyTurnDelegate e_turnDelegate; // goes in enemy
-
     void Awake()
     {
         g_global = S_Global.Instance;
@@ -50,27 +47,24 @@ public class S_Enemy : MonoBehaviour
         // Grab and set default scale
         e_v3_defaultScale = gameObject.transform.localScale;
 
-        // Left over code, leaving
-        //a_audioPlayer = GameObject.Find("/Audio/Sound Effects/Attack/Vanilla");
-
         g_global.g_ls_activeEnemies.Add(this);
     }
 
     private void Start()
     {
-        //If enemy 1 move spots
+        //If enemy 1, move spots
         if (e_i_enemyCount == 1)
         {
             gameObject.transform.SetParent(g_global.g_e_enemyPosition1.transform, false);
         }
 
-        //If enemy 2 move spots
+        //If enemy 2, move spots
         if (e_i_enemyCount == 2)
         {
             gameObject.transform.SetParent(g_global.g_e_enemyPosition2.transform, false);
         }
 
-        //If enemy 3 move spots
+        //If enemy 3, move spots
         if (e_i_enemyCount == 3)
         {
             gameObject.transform.SetParent(g_global.g_e_enemyPosition3.transform, false);
@@ -78,94 +72,16 @@ public class S_Enemy : MonoBehaviour
 
         // Need to add in enemy 4 and 5 placement and the scene knowing if it should have 3 enemies or 5 enemies from S_GameManager or even get something set initially as a bool in S_Global we toggle
         // - Josh
-
-        SetDelegate();
     }
 
-    void SetCount()
+    /// <summary>
+    /// Determine the Enemy Number
+    /// - Josh
+    /// </summary>
+    private void SetCount()
     {
         g_global.g_i_enemyCount += 1;
         e_i_enemyCount = g_global.g_i_enemyCount;
-        return; 
-    }
-
-    /// <summary>
-    /// Set the delegate of the enemy to the pregrabbed delegate already on it
-    /// </summary>
-    public void SetDelegate()
-    {
-        if (e_i_enemyCount == 1)
-        {
-            S_EventManager.e_enemy1PhaseEvent += e_turnDelegate;
-        }
-        else if (e_i_enemyCount == 2)
-        {
-            S_EventManager.e_enemy2PhaseEvent += e_turnDelegate;
-        }
-        else if (e_i_enemyCount == 3)
-        {
-            S_EventManager.e_enemy3PhaseEvent += e_turnDelegate;
-        }
-        else if (e_i_enemyCount == 4)
-        {
-            S_EventManager.e_enemy4PhaseEvent += e_turnDelegate;
-        }
-        else if (e_i_enemyCount == 5)
-        {
-            S_EventManager.e_enemy5PhaseEvent += e_turnDelegate;
-        }
-    }
-
-    /// <summary>
-    /// Set the turn state for the enemy, from the enemy
-    /// Check to see if already done
-    /// - Josh
-    /// </summary>
-    public void SetTurnState()
-    {
-        if (g_global.g_enemyState.EnemyStateCheck(e_i_enemyCount) == true)
-        {
-            if(e_i_enemyCount == 1)
-            {
-                if(S_EventManager.e_b_enemy1PhaseNull == false)
-                {
-                    S_EventManager.e_enemy1PhaseEvent += g_global.g_turnManager.OverallEnemyTurn;
-                    S_EventManager.e_b_enemy1PhaseNull = true;
-                }
-            }
-            else if(e_i_enemyCount == 2)
-            {
-                if(S_EventManager.e_b_enemy2PhaseNull == false)
-                {
-                    S_EventManager.e_enemy2PhaseEvent += g_global.g_turnManager.OverallEnemyTurn;
-                    S_EventManager.e_b_enemy2PhaseNull = true;
-                }
-            }
-            else if (e_i_enemyCount == 3)
-            {
-                if(S_EventManager.e_b_enemy3PhaseNull == false)
-                {
-                    S_EventManager.e_enemy3PhaseEvent += g_global.g_turnManager.OverallEnemyTurn;
-                    S_EventManager.e_b_enemy3PhaseNull = true;
-                }
-            }
-            else if (e_i_enemyCount == 4)
-            {
-                if(S_EventManager.e_b_enemy4PhaseNull == false)
-                {
-                    S_EventManager.e_enemy4PhaseEvent += g_global.g_turnManager.OverallEnemyTurn;
-                    S_EventManager.e_b_enemy4PhaseNull = true;
-                }
-            }
-            else if (e_i_enemyCount == 5)
-            {
-                if(S_EventManager.e_b_enemy5PhaseNull == false)
-                {
-                    S_EventManager.e_enemy5PhaseEvent += g_global.g_turnManager.OverallEnemyTurn;
-                    S_EventManager.e_b_enemy5PhaseNull = true;
-                }
-            }
-        }
     }
 
     /// <summary>
@@ -418,6 +334,65 @@ public class S_Enemy : MonoBehaviour
         else 
         {
             e_sc_enemyAttributes.e_highlightCircle.SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// Function for enemy action within their turn phase to be triggered
+    /// Now in S_Enemy  
+    /// - Josh
+    /// </summary>
+    /// <param name="_enemyNum"></param>
+    /// <returns></returns>
+    public IEnumerator EnemyTurnAction(int _enemyNum)
+    {
+        if (!g_global.g_enemyState.EnemySkipTurnCheck(_enemyNum))
+        {
+            // Declare Turn for UI
+            g_global.g_enemyState.DeclareCurrentTurn(_enemyNum);
+
+            //Do your action
+            if (g_global.g_turnManager.GetEnemyAction(_enemyNum) == 6) // Check shielding
+            {
+                EnemyShielded(g_global.g_enemyState.GetEnemyDataSheet(_enemyNum).e_str_enemyType, g_global.g_enemyState.GetEnemyDataSheet(_enemyNum).e_i_shieldMax);
+            }
+            else if (g_global.g_turnManager.GetEnemyAction(_enemyNum) == 7) // Check attacking
+            {
+                //play enemy animation
+                g_global.g_enemyState.GetEnemyDataSheet(_enemyNum).e_a_animator.Play("attack");
+
+                g_global.g_player.PlayerAttacked(g_global.g_enemyState.GetEnemyDataSheet(_enemyNum).e_i_enemyDamageValue);
+
+                //Then play sounds
+                if (g_global.g_enemyState.GetEnemyDataSheet(_enemyNum).e_str_enemyType == "Beast")
+                {
+                    FMODUnity.RuntimeManager.PlayOneShot("event:/Sounds/Attack & Ability/Attack_Vanilla");
+                }
+                else if (g_global.g_enemyState.GetEnemyDataSheet(_enemyNum).e_str_enemyType == "Magician")
+                {
+                    FMODUnity.RuntimeManager.PlayOneShot("event:/Jager G421/attack-magic");
+                }
+                else if (g_global.g_enemyState.GetEnemyDataSheet(_enemyNum).e_str_enemyType == "Brawler")
+                {
+                    FMODUnity.RuntimeManager.PlayOneShot("event:/Jager G421/attack-magic");
+                    FMODUnity.RuntimeManager.PlayOneShot("event:/Sounds/Attack & Ability/Attack_Vanilla");
+                }
+            }
+            else if (g_global.g_turnManager.GetEnemyAction(_enemyNum) == 8) // Check special ability
+            {
+                g_global.g_enemyState.GetEnemyScript(_enemyNum).EnemySpecialAbility(g_global.g_enemyState.GetEnemyDataSheet(_enemyNum).e_str_enemyType);
+            }
+
+            yield return new WaitForSeconds(4);
+
+            // Set the active enemy bool to clear so the next enemy can do their turn
+            g_global.g_turnManager.e_b_enemyIsActive = true;
+            Debug.Log("Returned!");
+        }
+        else
+        {
+            Debug.Log("Enemy " + _enemyNum + "'s turn is skipped!");
+            yield return new WaitForSeconds(4);
         }
     }
 
