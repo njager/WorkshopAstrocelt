cfc44838f8dfca3bd107af3f6c08dc5c2af22eae
Joshua Tonner <jtonner@iu.edu>
2022-08-31 13:06:54 -0400

    Map scripts created

---
+++ WorkshopAstroceltProject/Assets/Scripts/Maps/S_MapChunkTemplate.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Map Chunk", menuName = "Map Chunk")]
+public class S_MapChunkTemplate : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
