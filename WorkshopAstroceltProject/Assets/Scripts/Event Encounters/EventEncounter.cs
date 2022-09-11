d39aff016ccea0e186ea0b238bbda86fc5e21b28
rifhall <riley@kjhalloran.com>
2022-09-11 16:40:16 -0400

    event encounter scripted out

--- WorkshopAstroceltProject/Assets/Scripts/Event Encounters/EventEncounter.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Event Encounters/EventEncounter.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 [System.Serializable]
 public class EventEncounter 
 {
+    [Header("Name for the Event")]
+    [TextArea(3, 10)]
     public string s_eventName;
 
+    [Header("Text description of the event")]
+    [TextArea(3, 10)]
     public string s_eventSummary;
 
     public Response[] ls_responses;
--- WorkshopAstroceltProject/Assets/Scripts/Event Encounters/Response.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Event Encounters/Response.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class Response : MonoBehaviour
+[CreateAssetMenu(fileName = "New Respose", menuName = "Response")]
+public class Response : ScriptableObject
 {
+    [Header("Text for the response")]
+    [TextArea(3, 10)]
     public string s_responseText;
 
     public Reward r_reward;
 
+    [Header("Percent chance that the player fails")]
     public float f_failureChance;
 
+    [Header("Text if Player fails")]
+    [TextArea(3, 10)]
     public string s_failureText;
 }
