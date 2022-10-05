42ab36783770a366332558e460b72f3eb3007e9e
agotetyx <70243580+agotetyx@users.noreply.github.com>
2022-10-05 17:55:26 -0400

    worked on dynamic resource bar

--- WorkshopAstroceltProject/Assets/Scripts/System/S_GameManager.cs
+++ WorkshopAstroceltProject/Assets/Scripts/System/S_GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class S_GameManager : MonoBehaviour
 {
@@ -19,7 +20,24 @@ public class S_GameManager : MonoBehaviour
 
     public int i_bones = 0;
 
-        //Status Effects
+    public SpriteRenderer Skull1;
+    public SpriteRenderer Skull1_Crack;
+    public SpriteRenderer Skull1_Selector;
+
+    public SpriteRenderer Skull2;
+    public SpriteRenderer Skull2_Crack;
+    public SpriteRenderer Skull2_Selector;
+
+    public SpriteRenderer SkullBoss;
+    //public SpriteRenderer SkullBoss_Crack;
+    public SpriteRenderer SkullBoss_Selector;
+
+    public SpriteRenderer encounterSprite;
+    public SpriteRenderer encounterSprite_Selector;
+
+    public Scene scene;
+
+    //Status Effects
     public bool b_bleeding = false;
     public bool b_resistant = false;
     public bool b_stunned = false;
@@ -36,6 +54,99 @@ public class S_GameManager : MonoBehaviour
         GameObject.DontDestroyOnLoad(this.gameObject);
     }
 
+    public void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+        //Debug.Log("Active Scene is '" + scene.name + "'."); */
+    }
+
+    public void Update()
+    {
+       if (scene.name == "Scn_1stEnemyEncounter")
+        {
+            Skull1_Crack.enabled = false;
+
+            Skull2.enabled = true;
+            Skull2_Crack.enabled = false;
+            Skull2_Selector.enabled = false;
+
+            SkullBoss.enabled = true;
+            SkullBoss_Selector.enabled = false;
+
+            encounterSprite.enabled = true;
+            encounterSprite_Selector.enabled = false;
+        }
+
+        if (scene.name == "Scn_2ndEnemyEncounter")
+        {
+            Skull1_Crack.enabled = true;
+            Skull1.enabled = false;
+            Skull1_Selector.enabled = false;
+
+            Skull2.enabled = true;
+            Skull2_Crack.enabled = false;
+            Skull2_Selector.enabled = true;
+
+            SkullBoss.enabled = true;
+            SkullBoss_Selector.enabled = false;
+
+            encounterSprite.enabled = false;
+            encounterSprite.enabled = false;
+        }
+
+        if (scene.name == "Scn_4thEnemyEncounter")
+        {
+            Skull1_Crack.enabled = true;
+            Skull1.enabled = false;
+            Skull1_Selector.enabled = false;
+
+            Skull2.enabled = false;
+            Skull2_Crack.enabled = true;
+            Skull2_Selector.enabled = false;
+
+            SkullBoss.enabled = true;
+            SkullBoss_Selector.enabled = true;
+
+            encounterSprite.enabled = false;
+            encounterSprite.enabled = false;
+        }
+
+        if (scene.name == "Scn_4thEnemyEncounter")
+        {
+            Skull1_Crack.enabled = true;
+            Skull1.enabled = false;
+            Skull1_Selector.enabled = false;
+
+            Skull2.enabled = false;
+            Skull2_Crack.enabled = true;
+            Skull2_Selector.enabled = false;
+
+            SkullBoss.enabled = true;
+            SkullBoss_Selector.enabled = true;
+
+            encounterSprite.enabled = false;
+            encounterSprite.enabled = false;
+        }
+
+        if (scene.name == "Event1-Bog" || scene.name == "Event2-Mushrooms" || scene.name == "Event3-Victory")
+        {
+            Skull1_Crack.enabled = true;
+            Skull1.enabled = false;
+            Skull1_Selector.enabled = false;
+
+            Skull2.enabled = false;
+            Skull2_Crack.enabled = true;
+            Skull2_Selector.enabled = false;
+
+            SkullBoss.enabled = false;
+            SkullBoss_Selector.enabled = false;
+
+            encounterSprite.enabled = true;
+            encounterSprite.enabled = true;
+        }
+
+
+    }
 
 
     //Getters and Setters//
