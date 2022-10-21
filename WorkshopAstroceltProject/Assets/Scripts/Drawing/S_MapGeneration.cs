00c6ea853b0b6f92f4bbbc70edb1eb35eb0e9dd1
ncthoman <ncthoman@icloud.com>
2022-10-21 14:08:54 -0400

    overlap bug mostly fixed
    edge cases need checked

--- WorkshopAstroceltProject/Assets/Scripts/Drawing/S_MapGeneration.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Drawing/S_MapGeneration.cs
@@ -32,29 +32,62 @@ public class S_MapGeneration : MonoBehaviour
         RandomMapSelector();
     }
 
+    public Vector3 RandomVector() {
+        bool check = true;
+        Vector3 goodvec = new Vector3(0,0,0); 
+        while (check)
+        {
+            float rand_x = Random.Range(-12, 12);
+            float rand_y = Random.Range(-3, 6);
+            Vector3 temp = new Vector3(rand_x, rand_y, 0);
+            goodvec = temp;
+            check = pos_checker(temp);
+        }
+        return goodvec;
+    }
+
+
+    public bool pos_checker(Vector3 potential_vector)
+    {
+        bool check_bool = true;
+        foreach (Transform g in map1.GetComponentsInChildren<Transform>())
+        {
+            if (Mathf.Abs(g.transform.position.x - potential_vector.x) <= .7)
+            {
+                check_bool = false;
+            }
+            if (Mathf.Abs(g.transform.position.y - potential_vector.y) <= .7)
+            {
+                check_bool = false;
+            }
+
+        }
+        return check_bool;
+        
+    }
+
     /// <summary>
     /// Chooses maps randomly, with no direct repeats
     /// Though yes it's dirty
     /// - Josh
     /// </summary>
+    /// 
     public void RandomMapSelector()
     {
         foreach (Transform g in map1.GetComponentsInChildren<Transform>())
         {
-            
-            float rand_x = Random.Range(-12, 12);
-            float rand_y = Random.Range(-3, 6);
-
-            Vector3 temp = new Vector3(rand_x, rand_y, 0);
-            g.transform.position = temp;
+            g.transform.position = RandomVector();
         }
-        //RunSpringGen();
+        RunSpringGen();
     }
+
+
+
     public void RunSpringGen()
     {
         foreach (Transform g in map1.GetComponentsInChildren<Transform>())
         {
-
+            //remove spring componentS
         }
     }
 }
