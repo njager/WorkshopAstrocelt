6b1d3daaa05386e874a92c1e6bdeedcf4bac42b0
Joshua Tonner <jtonner@iu.edu>
2022-11-09 18:19:12 -0500

    Keeping for posterity

--- WorkshopAstroceltProject/Assets/Scripts/Audio/S_AudioManager.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Audio/S_AudioManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class S_AudioManager : MonoBehaviour
 {
+    /////////////////////////////--------------\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ 
+    ///////////////////////////// Script Setup \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ 
+    /////////////////////////////--------------\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
     private S_Global g_global;
 
     [Header("Combat Scene?")]
@@ -15,30 +18,63 @@ public class S_AudioManager : MonoBehaviour
     [Header("Random Number for Integer")]
     public float f_randomFloatValue;
 
+    [Header("Player Health")]
+    [SerializeField] float p_f_playerHealth;
+
+    [Header("Player Health Max")]
+    [SerializeField] float p_f_playerMaxHealth;
+
     [Header("Boss Health")]
-    public float e_f_enemyHealth;
+    [SerializeField] float e_f_bossHealth;
 
     [Header("Boss Health Max")]
-    public float e_f_enemyMaxHealth;
+    [SerializeField] float e_f_bossMaxHealth;
+
+    [Header("Enemies Health Values")]
+    [SerializeField] float e_f_enemy1Health;
+    [SerializeField] float e_f_enemy2Health;
+    [SerializeField] float e_f_enemy3Health;
+    [SerializeField] float e_f_enemy4Health;
+    [SerializeField] float e_f_enemy5Health;
+
+    [Header("Enemies Max Health Values")]
+    [SerializeField] float e_f_enemy1MaxHealth;
+    [SerializeField] float e_f_enemy2MaxHealth;
+    [SerializeField] float e_f_enemy3MaxHealth;
+    [SerializeField] float e_f_enemy4MaxHealth;
+    [SerializeField] float e_f_enemy5MaxHealth;
+
+    [Header("Player Percentage Values")]
+    public float e_f_playerAudioPercentage;
+        
+    [Header("Enemy Percentage Values")]
+    public float e_f_enemy1AudioPercentage;
+    public float e_f_enemy2AudioPercentage;
+    public float e_f_enemy3AudioPercentage;
+    public float e_f_enemy4AudioPercentage;
+    public float e_f_enemy5AudioPercentage;
+
+    [Header("Boss Percentage Values")]
+    public float e_f_bossAudioPercentage;
+
+    /////////////////////////////---------\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ 
+    ///////////////////////////// Methods \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ 
+    /////////////////////////////---------\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
     private void Awake()
     {
         g_global = S_Global.Instance;
+    }
 
-        // Set the random values
-        float _randomInt1 = GetRandomFloatNumber();
-        float _randomInt2 = GetRandomFloatNumber();
-
-        // Add them to enemy health
-        e_f_enemyHealth = g_global.g_enemyState.e_bossEnemy.GetComponent<S_EnemyAttributes>().GetEnemyHealthValue() + _randomInt1;
-        e_f_enemyMaxHealth = g_global.g_enemyState.e_bossEnemy.GetComponent<S_EnemyAttributes>().GetEnemyMaxHealthValue() + _randomInt2;
+    private void Start()
+    {
+        CalculateAudioPercentages(); 
     }
 
     private void Update()
     {
         f_timeInScene += Time.deltaTime;
     }
-
 
     /// <summary>
     /// Helper to set the random float value to a randomly pulled valued
@@ -69,5 +105,98 @@ public class S_AudioManager : MonoBehaviour
     {
         ResetRandomValue();
         return f_randomFloatValue;
+    }
+
+    /// <summary>
+    /// Calculate the audio percentage for Victor to use based off enemy values
+    /// </summary>
+    public void CalculateAudioPercentages()
+    {
+        /// Calculate Boss Audio Percentage \\\
+        
+        // Set the random values
+        float _randomInt1 = GetRandomFloatNumber();
+        float _randomInt2 = GetRandomFloatNumber();
+
+        // Add them to enemy health
+        e_f_bossHealth = g_global.g_enemyState.e_bossEnemy.GetComponent<S_EnemyAttributes>().GetEnemyHealthValue() + _randomInt1;
+        e_f_bossMaxHealth = g_global.g_enemyState.e_bossEnemy.GetComponent<S_EnemyAttributes>().GetEnemyMaxHealthValue() + _randomInt2;
+
+        // Calculate and Set the Percentage
+        float _temp1 = e_f_bossHealth / e_f_bossMaxHealth;
+        e_f_bossAudioPercentage = _temp1 * 100;
+
+
+        ////// Calculate Enemy Audio Values \\\\\\
+
+        //// Enemy 1 \\\\
+
+        // Set the random values
+        float _randomInt3 = GetRandomFloatNumber();
+        float _randomInt4 = GetRandomFloatNumber();
+
+        // Add them to enemy health
+        e_f_enemy1Health = g_global.g_enemyAttributeSheet1.GetEnemyHealthValue() + _randomInt3;
+        e_f_enemy1MaxHealth = g_global.g_enemyAttributeSheet1.GetEnemyMaxHealthValue() + _randomInt4;
+
+        // Calculate and Set the Percentage
+        float _temp2 = e_f_enemy1Health / e_f_enemy1MaxHealth;
+        e_f_enemy1AudioPercentage = _temp2 * 100;
+
+        //// Enemy 2 \\\\
+
+        // Set the random values
+        float _randomInt5 = GetRandomFloatNumber();
+        float _randomInt6 = GetRandomFloatNumber();
+
+        // Add them to enemy health
+        e_f_enemy2Health = g_global.g_enemyAttributeSheet2.GetEnemyHealthValue() + _randomInt5;
+        e_f_enemy2MaxHealth = g_global.g_enemyAttributeSheet2.GetEnemyMaxHealthValue() + _randomInt6;
+
+        // Calculate and Set the Percentage
+        float _temp3 = e_f_enemy2Health / e_f_enemy2MaxHealth;
+        e_f_enemy2AudioPercentage = _temp3 * 100;
+
+        //// Enemy 3 \\\\
+
+        // Set the random values
+        float _randomInt7 = GetRandomFloatNumber();
+        float _randomInt8 = GetRandomFloatNumber();
+
+        // Add them to enemy health
+        e_f_enemy3Health = g_global.g_enemyAttributeSheet3.GetEnemyHealthValue() + _randomInt7;
+        e_f_enemy3MaxHealth = g_global.g_enemyAttributeSheet3.GetEnemyMaxHealthValue() + _randomInt8;
+
+        // Calculate and Set the Percentage
+        float _temp4 = e_f_enemy3Health / e_f_enemy3MaxHealth;
+        e_f_enemy3AudioPercentage = _temp4 * 100;
+
+        //// Enemy 4 \\\\
+
+        // Set the random values
+        float _randomInt9 = GetRandomFloatNumber();
+        float _randomInt10 = GetRandomFloatNumber();
+
+        // Add them to enemy health
+        e_f_enemy4Health = g_global.g_enemyAttributeSheet4.GetEnemyHealthValue() + _randomInt9;
+        e_f_enemy4MaxHealth = g_global.g_enemyAttributeSheet4.GetEnemyMaxHealthValue() + _randomInt10;
+
+        // Calculate and Set the Percentage
+        float _temp5 = e_f_enemy4Health / e_f_enemy4MaxHealth;
+        e_f_enemy4AudioPercentage = _temp5 * 100;
+
+        //// Enemy 5 \\\\
+
+        // Set the random values
+        float _randomInt11 = GetRandomFloatNumber();
+        float _randomInt12 = GetRandomFloatNumber();
+
+        // Add them to enemy health
+        e_f_enemy5Health = g_global.g_enemyAttributeSheet5.GetEnemyHealthValue() + _randomInt11;
+        e_f_enemy5MaxHealth = g_global.g_enemyAttributeSheet5.GetEnemyMaxHealthValue() + _randomInt12;
+
+        // Calculate and Set the Percentage
+        float _temp6 = e_f_enemy5Health / e_f_enemy5MaxHealth;
+        e_f_enemy5AudioPercentage = _temp6 * 100;
     }
 }
