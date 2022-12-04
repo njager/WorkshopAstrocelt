16a7b1856e51756147995b578b95b1681dbf17de
Joshua Tonner <jtonner@iu.edu>
2022-12-04 17:40:27 -0500

    Coming together for health bar effects

--- WorkshopAstroceltProject/Assets/Scripts/UI/S_HealthBarStatusEffects.cs
+++ WorkshopAstroceltProject/Assets/Scripts/UI/S_HealthBarStatusEffects.cs
@@ -9,10 +9,10 @@ public class S_HealthBarStatusEffects : MonoBehaviour
     private S_Global g_global;
 
     [Header("Health Bar Status Effects")]
-    [SerializeField] Image chg_UI_acidicStatusEffect;
-    [SerializeField] Image chg_UI_bleedingStatusEffect;
-    [SerializeField] Image chg_UI_resistantStatusEffect;
-    [SerializeField] Image chg_UI_stunnedStatusEffect;
+    [SerializeField] GameObject chg_UI_acidicStatusEffect;
+    [SerializeField] GameObject chg_UI_bleedingStatusEffect;
+    [SerializeField] GameObject chg_UI_resistantStatusEffect;
+    [SerializeField] GameObject chg_UI_stunnedStatusEffect;
 
     [Header("Position GameObjects")]
     [SerializeField] GameObject chg_statusEffectPosition1;
@@ -38,6 +38,13 @@ public class S_HealthBarStatusEffects : MonoBehaviour
     [SerializeField] S_TooltipTemplate chg_tl_bleedingStatusEffectTooltip;
     [SerializeField] S_TooltipTemplate chg_tl_resistantStatusEffectTooltip;
     [SerializeField] S_TooltipTemplate chg_tl_stunnedStatusEffectTooltip;
+
+    [Header("Occupied Slot Count")]
+    [SerializeField] int chg_i_slotsOccupied;
+
+    [Header("Float Values")]
+    [SerializeField] float chg_f_spawnMoveValue;
+    [SerializeField] float chg_f_endMoveValue;
 
     private void Awake()
     {
@@ -74,38 +81,140 @@ public class S_HealthBarStatusEffects : MonoBehaviour
     /// using int for clarity
     /// - Josh
     /// </summary>
-    public void EndEffect(int _positionNum)
+    public void EndStatusEffect(int _positionNum)
     {
         if(_positionNum == 1)
         {
-            chg_statusEffectPosition1Child.transform.DOMove();
-            chg_UI_statusEffectPosition1
+            chg_statusEffectPosition1Child.transform.DOMove(chg_statusEffectSpawn.transform.position, chg_f_endMoveValue);
         }
         else if(_positionNum == 2)
         {
-
+            chg_statusEffectPosition2Child.transform.DOMove(chg_statusEffectSpawn.transform.position, chg_f_endMoveValue);
+        }
+        else if (_positionNum == 3)
+        {
+            chg_statusEffectPosition3Child.transform.DOMove(chg_statusEffectSpawn.transform.position, chg_f_endMoveValue);
+        }
+        else if (_positionNum == 4)
+        {
+            chg_statusEffectPosition4Child.transform.DOMove(chg_statusEffectSpawn.transform.position, chg_f_endMoveValue);
         }
     }
 
-    public void SetPosition(Image _image, int _positionNum)
+    /// <summary>
+    /// Set the position for a given Status Effect
+    /// </summary>
+    /// <param name="_image"></param>
+    public void AddStatusEffect(string _effect)
     {
-        if(_positionNum == 1)
+        if (_effect.Equals("acid"))
         {
-            
+            if (chg_i_slotsOccupied == 0)
+            {
+                chg_UI_acidicStatusEffect.transform.SetParent(chg_statusEffectPosition1.transform, true);
+                chg_statusEffectPosition1Child = chg_UI_acidicStatusEffect;
+                chg_str_position1Identifier = "acid";
+            }
+            else if (chg_i_slotsOccupied == 1)
+            {
+                chg_UI_acidicStatusEffect.transform.SetParent(chg_statusEffectPosition2.transform, true);
+                chg_statusEffectPosition2Child = chg_UI_acidicStatusEffect;
+                chg_str_position2Identifier = "acid";
+            }
+            else if (chg_i_slotsOccupied == 2)
+            {
+                chg_UI_acidicStatusEffect.transform.SetParent(chg_statusEffectPosition3.transform, true);
+                chg_statusEffectPosition3Child = chg_UI_acidicStatusEffect;
+                chg_str_position3Identifier = "acid";
+            }
+            else if (chg_i_slotsOccupied == 3)
+            {
+                chg_UI_acidicStatusEffect.transform.SetParent(chg_statusEffectPosition4.transform, true);
+                chg_statusEffectPosition4Child = chg_UI_acidicStatusEffect;
+                chg_str_position4Identifier = "acid";
+            }
         }
-        else if(_positionNum == 2)
+        else if (_effect.Equals("bleed"))
         {
+            if (chg_i_slotsOccupied == 0)
+            {
+                chg_UI_bleedingStatusEffect.transform.SetParent(chg_statusEffectPosition1.transform, true);
+                chg_statusEffectPosition1Child = chg_UI_bleedingStatusEffect;
+                chg_str_position1Identifier = "bleed";
+            }
+            else if (chg_i_slotsOccupied == 1)
+            {
+                chg_UI_bleedingStatusEffect.transform.SetParent(chg_statusEffectPosition2.transform, true);
+                chg_statusEffectPosition2Child = chg_UI_bleedingStatusEffect;
+                chg_str_position2Identifier = "bleed";
+            }
+            else if (chg_i_slotsOccupied == 2)
+            {
+                chg_UI_bleedingStatusEffect.transform.SetParent(chg_statusEffectPosition3.transform, true);
+                chg_statusEffectPosition3Child = chg_UI_bleedingStatusEffect;
+                chg_str_position3Identifier = "bleed";
+            }
+            else if (chg_i_slotsOccupied == 3)
+            {
+                chg_UI_bleedingStatusEffect.transform.SetParent(chg_statusEffectPosition4.transform, true);
+                chg_statusEffectPosition4Child = chg_UI_bleedingStatusEffect;
+                chg_str_position4Identifier = "bleed";
+            }
+        }
+        else if (_effect.Equals("resist"))
+        {
+            if (chg_i_slotsOccupied == 0)
+            {
+                chg_UI_resistantStatusEffect.transform.SetParent(chg_statusEffectPosition1.transform, true);
+                chg_statusEffectPosition1Child = chg_UI_resistantStatusEffect;
+                chg_str_position1Identifier = "resist";
+            }
+            else if (chg_i_slotsOccupied == 1)
+            {
+                chg_UI_resistantStatusEffect.transform.SetParent(chg_statusEffectPosition2.transform, true);
+                chg_statusEffectPosition2Child = chg_UI_resistantStatusEffect;
+                chg_str_position2Identifier = "resist";
+            }
+            else if (chg_i_slotsOccupied == 2)
+            {
+                chg_UI_resistantStatusEffect.transform.SetParent(chg_statusEffectPosition3.transform, true);
+                chg_statusEffectPosition3Child = chg_UI_resistantStatusEffect;
+                chg_str_position3Identifier = "resist";
+            }
+            else if (chg_i_slotsOccupied == 3)
+            {
+                chg_UI_resistantStatusEffect.transform.SetParent(chg_statusEffectPosition4.transform, true);
+                chg_statusEffectPosition4Child = chg_UI_resistantStatusEffect;
+                chg_str_position4Identifier = "resist";
+            }
+        }
+        else if (_effect.Equals("stun"))
+        {
+            if (chg_i_slotsOccupied == 0)
+            {
+                chg_UI_stunnedStatusEffect.transform.SetParent(chg_statusEffectPosition1.transform, true);
+                chg_statusEffectPosition1Child = chg_UI_stunnedStatusEffect;
+                chg_str_position1Identifier = "stun";
+            }
+            else if (chg_i_slotsOccupied == 1)
+            {
+                chg_UI_stunnedStatusEffect.transform.SetParent(chg_statusEffectPosition2.transform, true);
+                chg_statusEffectPosition2Child = chg_UI_stunnedStatusEffect;
+                chg_str_position2Identifier = "stun";
+            }
+            else if (chg_i_slotsOccupied == 2)
+            {
+                chg_UI_stunnedStatusEffect.transform.SetParent(chg_statusEffectPosition3.transform, true);
+                chg_statusEffectPosition3Child = chg_UI_stunnedStatusEffect;
+                chg_str_position3Identifier = "stun";
+            }
+            else if (chg_i_slotsOccupied == 3)
+            {
+                chg_UI_stunnedStatusEffect.transform.SetParent(chg_statusEffectPosition4.transform, true);
+                chg_statusEffectPosition4Child = chg_UI_stunnedStatusEffect;
+                chg_str_position4Identifier = "stun";
+            }
+        }
 
-        }
-        else if(_positionNum == 3)
-        {
-
-        }
-        else if(_positionNum == 4)
-        {
-
-        }
     }
-
-    
 }
