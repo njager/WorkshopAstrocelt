7e849c822da44c1b386b03fcc1b4a84fc88a95a1
agotetyx <70243580+agotetyx@users.noreply.github.com>
2022-12-07 18:05:16 -0500

    implemented player flash on damage
    might not work

--- WorkshopAstroceltProject/Assets/Scripts/Characters/Player/S_Player.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Characters/Player/S_Player.cs
@@ -27,18 +27,47 @@ public class S_Player : MonoBehaviour
 
     public bool isComplete;
 
+    [Tooltip("Material to switch to during the flash.")]
+    [SerializeField] private Material flashMaterial;
+
+    [Tooltip("Duration of the flash.")]
+    [SerializeField] private float duration;
+
+
+    // The material that was in use, when the script started.
+    private Material originalMaterial;
+
+    // The currently running coroutine.
+    private Coroutine flashRoutine;
+
+
+    void Start()
+    {
+        // Get the SpriteRenderer to be used,
+        // alternatively you could set it from the inspector.
+        playerSprite = GetComponent<SpriteRenderer>();
+
+        // Get the material that the SpriteRenderer uses, 
+        // so we can switch back to it after the flash ended.
+        originalMaterial = playerSprite.material;
+
+        // Copy the flashMaterial material, this is needed, 
+        // so it can be modified without any side effects.
+        flashMaterial = new Material(flashMaterial);
+    }
+
     /// <summary>
     /// Basic Start S_Global setup, grabbing playerAttributes; 
     /// </summary>
 
-  /*  public void Update()
-    {
-        if (g_global.g_b_playerTurn == true && isComplete == false)
-        {
-            playerSprite.transform.DOShakePosition(1000f, new Vector3(0, 0.00001f, 1), 1, 0, false, false);
-            isComplete = true;
-        }
-    }*/
+    /*  public void Update()
+      {
+          if (g_global.g_b_playerTurn == true && isComplete == false)
+          {
+              playerSprite.transform.DOShakePosition(1000f, new Vector3(0, 0.00001f, 1), 1, 0, false, false);
+              isComplete = true;
+          }
+      }*/
 
     void Awake()
     {
@@ -63,6 +92,7 @@ public class S_Player : MonoBehaviour
             int _newDamageValue = (int)_damageValue / 2;
             if (p_sc_playerAttributes.GetPlayerShieldValue() <= 0)
             {
+                Flash(Color.blue);
                 // Calculate and set health values
                 int _newValue1 = p_sc_playerAttributes.GetPlayerHealthValue() - _newDamageValue;
                 p_sc_playerAttributes.SetPlayerHealthValue(_newValue1);
@@ -116,6 +146,8 @@ public class S_Player : MonoBehaviour
                 p_sc_playerAttributes.p_pe_blood.Play();
                 //Debug.Log("Player Attacked!");
 
+                Flash(Color.white);
+
                 //trigger a coroutine to change sprite and go back
                 StartCoroutine(ChangeDamageSprite());
             }
@@ -151,6 +183,38 @@ public class S_Player : MonoBehaviour
 
         // Update Player UI
         PlayerValuesLimitCheck();
+    }
+
+    public void Flash(Color color)
+    {
+        // If the flashRoutine is not null, then it is currently running.
+        if (flashRoutine != null)
+        {
+            // In this case, we should stop it first.
+            // Multiple FlashRoutines the same time would cause bugs.
+            StopCoroutine(flashRoutine);
+        }
+
+        // Start the Coroutine, and store the reference for it.
+        flashRoutine = StartCoroutine(FlashRoutine(color));
+    }
+
+    private IEnumerator FlashRoutine(Color color)
+    {
+        // Swap to the flashMaterial.
+        playerSprite.material = flashMaterial;
+
+        // Set the desired color for the flash.
+        flashMaterial.color = color;
+
+        // Pause the execution of this function for "duration" seconds.
+        yield return new WaitForSeconds(duration);
+
+        // After the pause, swap back to the original material.
+        playerSprite.material = originalMaterial;
+
+        // Set the flashRoutine to null, signaling that it's finished.
+        flashRoutine = null;
     }
 
     /// <summary>
