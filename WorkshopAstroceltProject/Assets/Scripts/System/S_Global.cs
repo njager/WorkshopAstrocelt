60000fc8c158f1037bbc3dbba6d43f009b1b094a
rifhall <riley@kjhalloran.com>
2022-12-09 02:35:16 -0500

    Reward Canvases added to scenes and random event encounters implemented

--- WorkshopAstroceltProject/Assets/Scripts/System/S_Global.cs
+++ WorkshopAstroceltProject/Assets/Scripts/System/S_Global.cs
@@ -39,6 +39,7 @@ public class S_Global : MonoBehaviour
     public S_TooltipManager g_tooltipManager;
     public S_VFXManager g_vfxManager;
     public S_AudioManager g_audioManager;
+    public S_RewardVisualScript g_rewardVisualScript;
 
     [Header("Character States")]
     public bool g_b_playerTurn;
