f0ad09883dd48091781f4f5c905180c08fb22387
Josh Tonner <jtonner@iu.edu>
2023-01-25 15:56:53 -0500

    Looking for old cardholder stuff now

--- WorkshopAstroceltProject/Assets/Scripts/Managers/S_MapElementManager.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Managers/S_MapElementManager.cs
@@ -12,7 +12,14 @@ public class S_MapElementManager : MonoBehaviour
     [SerializeField] GameObject mp_activeMap;
 
     [Header("Master Map List")]
-    [SerializeField] List<(Transform, int mp_i_masterListObjectID)> mp_ls_masterMapList = new List<(Transform, int)>();
+    [SerializeField] List<(List<(Transform, int mp_i_masterListObjectID)>, int mp_i_mapMasterListIndex)> mp_ls_masterMapList = new List<(List<(Transform, int)>, int)>();
+
+    [Header("Current Map Index")]
+    [SerializeField] int mp_i_currentMapID;
+
+    [Header("Previous Map Index")]
+    [SerializeField] int mp_i_previousMapID;
+
 
     [Header("Energy Star Prefab")]
     [SerializeField] GameObject s_energyStarPrefab; // Keeping for abilites that would add a star
@@ -26,6 +33,7 @@ public class S_MapElementManager : MonoBehaviour
 
     private void Start()
     {
+        mp_activeMap = g_global.g_newMapManager.activeMap;
         GenerateDynamicMapList();
     }
 
@@ -34,7 +42,7 @@ public class S_MapElementManager : MonoBehaviour
     /// Run the map generation list function to dynamically make maps
     /// - Josh
     /// </summary>
-    private void GenerateDynamicMapList()
+    private void GenerateDynamicMapList() // Do this every round with turn manager (place after Thoman figures out his logic calls)
     {
 
     }
@@ -43,12 +51,12 @@ public class S_MapElementManager : MonoBehaviour
     /// Helper function to get parent function to reparent newly added stars
     /// - Josh
     /// </summary>
-    /// <param name="_mapNum"></param>
+    /// <param name="_objectIndex"></param>
     /// <returns></returns>
     public Transform GetTransformFromObjectID(int _objectIndex)
     {
         Transform _transformToReturn = null;
-
+        foreach((List<Transform>, int))
         foreach ((Transform, int) _currentMapInstance in mp_ls_masterMapList.ToList())
         {
             if(_currentMapInstance.Item2 == _objectIndex) 
@@ -64,10 +72,25 @@ public class S_MapElementManager : MonoBehaviour
     /// Helper function to get map list
     /// - Josh 
     /// </summary>
-    /// <param name="_mapNum"></param>
+    /// <param name="_objectIndex"></param>
     /// <returns></returns>
     public List<S_StarClass> GetMapListObjectID(int _objectIndex)
     {
         return null;
+    }
+
+    public void AddStarToMapPreRun() 
+    {
+
+    }
+
+    public void AddStarToMapPostRun() // Pops a star into existence at a viable spot
+    {
+
+    }
+
+    public void RemoveStarFromMapPreRun() 
+    {
+
     }
 }
