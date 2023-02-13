b5473d77fa939e055678da0191b40b05c92fa35d
Josh Tonner <jtonner@iu.edu>
2023-02-13 17:14:28 -0500

    Methods to add or remove cards from the card stockpile

--- WorkshopAstroceltProject/Assets/Scripts/Card/S_CardStockpile.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Card/S_CardStockpile.cs
@@ -19,6 +19,7 @@ public class S_CardStockpile : MonoBehaviour
 
     /// <summary>
     /// When player clicks on the stockpile locator, swap between the stockpiled cards positions, bringing them to the forefront
+    /// - Josh
     /// </summary>
     public void OnClick() 
     {
@@ -26,25 +27,49 @@ public class S_CardStockpile : MonoBehaviour
         crd_i_stockpileClickCount += 1;
     }
 
+    /// <summary>
+    /// Method to Add a Card to the Stockpile
+    /// - Josh
+    /// </summary>
+    /// <param name="_card"></param>
     public void AddCardToStockpile(GameObject _card) 
     {
-        // Increment Index
+        // Increment Total Index
         crd_i_stockpileTotalIndex += 1;
 
         // Add card from list
         lst_crd_stockpile.Add((_card, crd_i_stockpileTotalIndex));
+
+        // Set Card's Stockpile Status in Card
+        _card.GetComponent<S_Card>().SetCardStockpileStatusBool(true);
+
+        // Set Card's Stockpile Index in Card
+        _card.GetComponent<S_Card>().SetCardStockpileListIndex(crd_i_stockpileTotalIndex);
     }
 
+    /// <summary>
+    /// Method to remove a Card from the stockpile
+    /// - Josh
+    /// </summary>
+    /// <param name="_card"></param>
     public void RemoveCardFromStockpile(GameObject _card) 
     {
-        // Increment Index
+        // Decrement Total Index
         crd_i_stockpileTotalIndex -= 1;
 
         // Find it's index through card
+        int _cardIndex = _card.GetComponent<S_Card>().GetCardStockpileListIndex(); 
 
         // Add card from list
-        lst_crd_stockpile.Remove(_card);
+        lst_crd_stockpile.Remove((_card, _cardIndex));
+
+        // Set Card's Stockpile Status in Card
+        _card.GetComponent<S_Card>().SetCardStockpileStatusBool(false);
+
+        // Set Card's Stockpile Index in Card
+        _card.GetComponent<S_Card>().SetCardStockpileListIndex(0);
     }
+    
 
     // Add boolean toggle in S_Card for if in stockpile or not
 
