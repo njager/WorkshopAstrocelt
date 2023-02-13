eb0725c664bcfa9e67f10620725aae1e95894854
Josh Tonner <jtonner@iu.edu>
2023-02-13 17:04:53 -0500

    Card stockpile bool toggle/check, list index inside the card, with getters and setters

--- WorkshopAstroceltProject/Assets/Scripts/Card/S_CardStockpile.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Card/S_CardStockpile.cs
@@ -1,33 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class S_CardStockpile : MonoBehaviour
 {
     [Header("Card Stockpile Click Count")]
-    public int crd_i_stockpileClickCount;
+    [SerializeField] int crd_i_stockpileClickCount;
+
+    [Header("Stockpile Total Index")]
+    [SerializeField] int crd_i_stockpileTotalIndex;
 
     [Header("Card Stockpile Positions")]
-    public GameObject crd_stockpileTopPosition;
+    [SerializeField] GameObject crd_stockpileTopPosition;
 
     [Header("Card Stockpile List")]
-    public List<GameObject> ls_crd_stockpile = new List<GameObject>();
+    [SerializeField] List<(GameObject, int)> lst_crd_stockpile = new List<(GameObject, int)>();
 
     /// <summary>
     /// When player clicks on the stockpile locator, swap between the stockpiled cards positions, bringing them to the forefront
     /// </summary>
     public void OnClick() 
     {
-
+        // Increment count for visual changes 
+        crd_i_stockpileClickCount += 1;
     }
 
-    private void AddCardToStockpile() 
+    public void AddCardToStockpile(GameObject _card) 
     {
+        // Increment Index
+        crd_i_stockpileTotalIndex += 1;
 
+        // Add card from list
+        lst_crd_stockpile.Add((_card, crd_i_stockpileTotalIndex));
     }
 
-    private void RemoveCardFromStockpile() 
+    public void RemoveCardFromStockpile(GameObject _card) 
     {
+        // Increment Index
+        crd_i_stockpileTotalIndex -= 1;
 
+        // Find it's index through card
+
+        // Add card from list
+        lst_crd_stockpile.Remove(_card);
     }
+
+    // Add boolean toggle in S_Card for if in stockpile or not
+
+    // Add check to now to toggle stockpile bool when cards get added and positions 1-5 are filled
 }
