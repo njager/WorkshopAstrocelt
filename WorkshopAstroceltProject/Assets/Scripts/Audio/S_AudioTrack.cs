3393e9a3df6a698beef73777852d76fc34b47799
agotetyx <70243580+agotetyx@users.noreply.github.com>
2023-04-02 21:23:26 -0400

    sfx slider being implemted

--- WorkshopAstroceltProject/Assets/Scripts/Audio/S_AudioTrack.cs
+++ WorkshopAstroceltProject/Assets/Scripts/Audio/S_AudioTrack.cs
@@ -79,6 +79,7 @@ public class S_AudioTrack : MonoBehaviour
         //vol = g_global.g_pauseMenu.musicVolume;
         var emitter = _sceneAudio.GetComponent<FMODUnity.StudioEventEmitter>();
         emitter.SetParameter("Volume", g_global.g_pauseMenu.musicVolume);
-        
+        emitter.SetParameter("SFX_Volume", g_global.g_pauseMenu.sfxVolume);
+
     }
 }
