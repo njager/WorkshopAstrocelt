c038924922444d5c8653b8d72b8e40c73cff3ad0
Josh Tonner <jtonner@iu.edu>
2023-04-04 14:39:27 -0400

    Some organization

--- WorkshopAstroceltProject/Assets/AudioTestScript2.cs
+++ WorkshopAstroceltProject/Assets/AudioTestScript2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestScript : MonoBehaviour
+public class AudioTestScript2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
