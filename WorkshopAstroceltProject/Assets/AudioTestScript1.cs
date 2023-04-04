58783cd82006d8670a9368c1a42f663a6f37cc7a
Josh Tonner <jtonner@iu.edu>
2023-04-04 14:43:43 -0400

    Emulating game manager

--- WorkshopAstroceltProject/Assets/AudioTestScript1.cs
+++ WorkshopAstroceltProject/Assets/AudioTestScript1.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class AudioTestScript1 : MonoBehaviour
+public class AudioTestScript1 : MonoBehaviour // Emulating GameManager Behaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static AudioTestScript1 Instance;
 
-    // Update is called once per frame
-    void Update()
+    public List<int> gm_ls_p_playerDeck;
+
+    public int i_playerHealth;
+
+    public int i_healthMax;
+
+    public int i_shield = 0;
+    public int i_shieldMax = 100;
+
+    public float f_playerEnergyGenerationRate = 1.0f;
+
+    public int i_bones = 0;
+
+    //Status Effects
+    public bool b_bleeding = false;
+    public bool b_resistant = false;
+    public bool b_stunned = false;
+
+    private void Awake()
     {
-        
+        if (Instance != null)
+        {
+            Debug.Log("We deleted ourself cuz old copy");
+            Destroy(this.gameObject);
+            return;
+        }
+        Debug.Log("We initialized");
+
+        //if the player restarts the game give them max health
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            i_playerHealth = i_healthMax;
+        }
+
+        Instance = this;
+        GameObject.DontDestroyOnLoad(this.gameObject);
     }
 }
